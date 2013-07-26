0b5bd739e4518d7c34e8010a7a3d8fcd38f48eee
slatenails <arezey@gmail.com>
2013-07-26 15:28:40 +0300

    some last cleanup to patchmaker

--- patchmaker.sh
+++ patchmaker.sh
@@ -7,13 +7,13 @@
 #
 # This is a rather intensive process but does show progess output.
 
-A_DIR=.PATCHMAKER_A_DIR
-B_DIR=.PATCHMAKER_B_DIR
-C_DIR=.PATCHMAKER_C_DIR
+a_dir=.PATCHMAKER_a_dir
+b_dir=.PATCHMAKER_b_dir
+c_dir=.PATCHMAKER_c_dir
 
 function cleanup {
 	echo "Cleaning up..."
-	rm -rf $A_DIR $B_DIR $C_DIR
+	rm -rf $a_dir $b_dir $c_dir
 }
 
 if [ -z "$3" ]; then
@@ -23,63 +23,66 @@ if [ -z "$3" ]; then
 	exit 1
 fi
 
+# We need clean temp directories first
 cleanup
 
+# Try extract the archives
 echo "Extracting archives..."
-unzip $1 -d $A_DIR >/dev/null && \
-	unzip $2 -d $B_DIR >/dev/null
+unzip $1 -d $a_dir >/dev/null && \
+	unzip $2 -d $b_dir >/dev/null
 
-if [ ! -d $A_DIR -o ! -d $B_DIR ]; then
+# If the archives could not be extracted, we can't continue.
+if [ ! -d $a_dir -o ! -d $b_dir ]; then
 	echo "Failed to extract $1 and $2!" >/dev/stderr
 	cleanup
 	exit 1
 fi
 
-mkdir $C_DIR
+# Enter the newer archive
+cd $b_dir
 
-cd $B_DIR
-
-i=0
-j=0
-FILES=$( find )
-NUMFILES=$( echo $FILES |wc -w )
+numprocessed=0
+numpatched=0
+files=$( find )
+numfiles=$( echo $files |wc -w )
 
 # Iterate through the files and see which ones have changed
-for f in $FILES; do
+for f in $files; do
+	# Skip any directories
 	if [ -d $f ]; then
-		# Directory - skip
-		let i+=1
+		let numprocessed+=1
 		continue;
 	fi
 	
 	# Generate MD5 sums of the two files. If they are different, we add this file
 	# to the new patch.
-	MD5A=$( md5sum ../$A_DIR/$f 2>/dev/null |awk '{printf $1}' )
-	MD5B=$( md5sum $f 2>/dev/null |awk '{printf $1}' )
+	md5_a=$( md5sum ../$a_dir/$f 2>/dev/null |awk '{printf $1}' )
+	md5_b=$( md5sum $f 2>/dev/null |awk '{printf $1}' )
 	
-	if [ "$MD5A" != "$MD5B" ]; then
+	if [ "$md5_a" != "$md5_b" ]; then
 		# Make sure that a directory exists for the file or cp will fail
-		mkdir -p ../$C_DIR/$( dirname $f )
+		mkdir -p ../$c_dir/$( dirname $f )
 		
 		# Copy the file now
-		cp $f ../$C_DIR/$f
+		cp $f ../$c_dir/$f
 		
-		let j+=1
+		let numpatched+=1
 	fi
 	
 	# Write some output to show progress
-	let i+=1
-	perc=$( expr $i \* 100 / $NUMFILES )
-	perc2=$( expr $j \* 100 / $NUMFILES )
-	echo -ne "\rProcessed: $i / $NUMFILES ($perc%); Patched: $j ($perc2%)"
+	let numprocessed+=1
+	processed_perc=$( expr $numprocessed \* 100 / $numfiles )
+	patched_perc=$( expr $numpatched \* 100 / $numfiles )
+	echo -ne "\rProcessed: $numprocessed / $numfiles ($processed_perc%); Patched: $numpatched ($patched_perc%)"
 done
 
 # Write a terminating newline
 echo
 
-cd ../$C_DIR
+cd ../$c_dir
 zip -r9 ../$3 *
 
+# Error if zip fails
 if [ "$?" != "0" ]; then
 	echo "Failed to create output file $3!" >/dev/stderr;
 	cd ..
